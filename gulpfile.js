fa74d1a5a8d26b31c980e4d5be46ff23b4c0558d
Tiago Porto <tiagoporto@ymail.com>
2014-05-12 16:01:23 -0300

    Otimized gulpfile
    Otimized gulp paths
    Add sass files to compile and watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@
 
 // Load plugins
 var gulp = require('gulp'),
-	download = require('gulp-download'),
 	uglify = require('gulp-uglify'),
 	imagemin = require('gulp-imagemin'),
 	rename = require('gulp-rename'),
@@ -22,67 +21,73 @@ var gulp = require('gulp'),
 	server = lr();
 
 // Path configs
-var	sass_path = 'src/stylesheets/**/*.scss', // .sass files
-	js_path   = 'src/scripts/**/*.js', // .js files
-	img_path  = 'src/images/**/*.{png,jpg,gif}'; // image files
+var public_path = 'project/', // public files
+	public_images = public_path + 'img', // optimized images
+	public_styles = public_path + 'css', // minified styles
+	public_scripts = public_path + 'js', // concat and minify scripts
+	sass_path = 'src/stylesheets/', // sass files
+	js_path   = 'src/scripts/', // js files
+	img_path  = 'src/images/'; // original image files
 
 
 //**********************************Tasks**********************************//
 
 // Optimize Images
 gulp.task('images', function() {
-	gulp.src([img_path, '!src/images/icons/*' ])
-		.pipe(imagemin({ optimizationLevel: 3, progressive: true, cache: true }))
-		.pipe(gulp.dest('project/img'))
+	gulp.src([img_path + '**/*.{png,jpg,gif}', '!' + img_path + '/icons/*'])
+		.pipe(imagemin({optimizationLevel: 4, progressive: true, cache: true}))
+		.pipe(gulp.dest(public_images))
 		.pipe(livereload(server))
-		.pipe(notify({ message: 'Images task complete' }));
+		.pipe(notify({message: 'Images task complete'}));
 });
 
 // Concat and Minify Scripts
 gulp.task('scripts', function() {
-	gulp.src(['src/scripts/libs/**', 'src/scripts/frameworks/**', 'src/scripts/plugins/**', 'src/scripts/onread/open_onread.js', js_path, 'src/scripts/onread/close_onread.js'])
+	gulp.src([js_path + '/libs/**',
+		      js_path + 'frameworks/**',
+		      js_path + 'plugins/**',
+		      js_path + 'onread/open_onread.js',
+		      js_path + '/**/*.js',
+		      js_path + 'onread/close_onread.js'])
 		.pipe(concat('main.js'))
 		.pipe(gulp.dest('project/js'))
 		.pipe(rename('main.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('project/js'))
+		.pipe(gulp.dest(public_scripts))
 		.pipe(livereload(server))
-		.pipe(notify({ message: 'Scripts task complete' }));
+		.pipe(notify({message: 'Scripts task complete'}));
 });
 
 // Compile Compass
 gulp.task('compass', function() {
-	gulp.src(sass_path)
+	gulp.src(sass_path + '**/*.{sass,scss}')
 		.pipe(compass({
 			project: path.join(__dirname, '/'),
-			css: 'project/css',
-			sass: 'src/stylesheets/',
-			image: 'src/images',
-
-			style: 'expanded',
-			//description:
-			//The output style for the compiled css.
-			//Nested, expanded, compact, or compressed.
-
+			css: public_styles,
+			sass: sass_path,
+			image: img_path,
+			style: 'expanded', //The output style for the compiled css. Nested, expanded, compact, or compressed.
 			comments: false,
 			relative: false,
 		}))
 		.pipe(livereload(server))
-		.pipe(notify({ message: 'Compass task complete' }));
+		.pipe(notify({message: 'Compass task complete'}));
 });
 
 // Clean Directories
 gulp.task('clean', function() {
-	return gulp.src(['project/css', 'project/js', 'project/img'], {read: false})
+	return gulp.src([public_styles,
+					 public_scripts,
+					 public_images], {read: false})
 		.pipe(clean())
-		.pipe(notify({ message: 'Clean task complete' }));
+		.pipe(notify({message: 'Clean task complete'}));
 });
 
 // Reload Browser
 gulp.task('reload-browser', function() {
-	gulp.src('project/**/*.html')
+	gulp.src(public_path + '**/*.{html,php}')
 		.pipe(livereload(server))
-		.pipe(notify({ message: 'Reload complete' }));
+		.pipe(notify({message: 'Reload complete'}));
 });
 
 // Watch
@@ -92,47 +97,27 @@ gulp.task('watch', function() {
 		if (err) return console.log(err);
 
 		// Watch .js files
-		gulp.watch(js_path, function(event) {
+		gulp.watch(js_path + '**/*.js', function(event) {
 			gulp.run('scripts');
 		});
 
-		// Watch .scss files
-		gulp.watch(sass_path, function(event) {
+		// Watch sass files
+		gulp.watch(sass_path + '**/*.{sass,scss}', function(event) {
 			gulp.run('compass');
 		});
 
 		// Watch .jpg .png .gif files
-		gulp.watch(img_path, function(event) {
+		gulp.watch(img_path + '**/*.{png,jpg,gif}', function(event) {
 		  gulp.run('images');
 		});
 
 		//Watch .html .php Files
-		gulp.watch('project/**/*.{html,php}', function(){
+		gulp.watch(public_path + '**/*.{html,php}', function(){
 			gulp.run('reload-browser');
 		});
 
 	});
 });
-
-// Download the latest version of jQuery
-gulp.task('jquery', function() {
-	download('http://code.jquery.com/jquery.js')
-    	.pipe(gulp.dest("src/scripts/libs"))
-    	.pipe(notify({ message: 'Jquery Copied' }));
-});
-
-// Download the latest version of jQuery UI
-gulp.task('jqueryui', function() {
-	download('http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.js')
-    	.pipe(gulp.dest("src/scripts/libs"))
-    	.pipe(notify({ message: 'Jquery UI Copied' }));
-});
-
-//Make task for jquery UI styles
-
-//Make task for jquery mobile
-
-//Make task for angular
 
 // Default task
 gulp.task('default', ['clean', 'compass', 'scripts', 'images'], function() {
