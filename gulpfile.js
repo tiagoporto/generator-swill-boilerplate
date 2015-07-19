78f3bdad097a4a17c2d320ff6774318633dc7584
Tiago Porto <tiago@tiagoporto.com>
2015-07-18 21:06:52 -0300

    Change version

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 /*
-*	Swill Boilerplate v3.0.0beta
+*	Swill Boilerplate v3.0.0
 *	https://github.com/tiagoporto/swill-boilerplate
 *	Copyright (c) 2014-2015 Tiago Porto (http://tiagoporto.com)
 *	Released under the MIT license
