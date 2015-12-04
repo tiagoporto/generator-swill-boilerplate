1524d403adde34b3c4a29f649fe01c425151ab47
Tiago Porto <tiago@tiagoporto.com>
2015-12-04 16:46:38 -0200

    Styles to form elements

--- src/scripts/settings/call_plugins.js
+++ src/scripts/settings/call_plugins.js
@@ -17,7 +17,7 @@ addLoadEvent(
         bgColor: '#f25648',
         color: '#ffffff',
         lowerThan: 'transform',
-        languagePath: 'lang/outdated_browser/en.html'
+        languagePath: 'lang/outdated_browser/{-}.html'
     })//,
 
 	//$('#logo').downloadTip({ 'position': 'right' })
