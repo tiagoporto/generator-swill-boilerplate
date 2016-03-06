16867635ac5414a17faf3464dae1a638c3001b84
Tiago Porto <tiagoporto@ymail.com>
2016-03-06 16:33:07 -0300

    Added strict

--- src/scripts/settings/call_plugins.js
+++ src/scripts/settings/call_plugins.js
@@ -1,24 +1,25 @@
 //event listener: DOM ready
 function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload !== 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			oldonload();
-			func();
-		};
-	}
+    'use strict';
+    var oldonload = window.onload;
+    if (typeof window.onload !== 'function') {
+        window.onload = func;
+    } else {
+        window.onload = function() {
+            oldonload();
+            func();
+        };
+    }
 }
 addLoadEvent(
-	//call plugins after DOM ready
+    //call plugins after DOM ready
 
-	outdatedBrowser({
-		bgColor: '#f25648',
-		color: '#fff',
-		lowerThan: 'transform',
-		languagePath: 'lang/outdated_browser/{-}.html'
-	}),
+    outdatedBrowser({
+        bgColor: '#f25648',
+        color: '#fff',
+        lowerThan: 'transform',
+        languagePath: 'lang/outdated_browser/{-}.html'
+    }),
 
-	$('#logo').downloadTip({ 'position': 'right' })
+    $('#logo').downloadTip({ 'position': 'right' })
 );
