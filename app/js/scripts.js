9ab6bc59a096310fcbf541a93280d99c3ed0de23
Tiago Porto <tiago@tiagoporto.com>
2016-09-18 00:16:11 -0300

    Added ui

---
+++ app/js/scripts.js
@@ -0,0 +1,11 @@
+/*!	Swill Boilerplate
+*	https://github.com/tiagoporto/generator-swill-boilerplate
+*	Copyright (c) 2014-2016 Tiago Porto (http://www.tiagoporto.com/)
+*	Released under the MIT license
+*/
+
+jQuery(document).ready(function($) {
+
+// JS
+"use strict";
+});
