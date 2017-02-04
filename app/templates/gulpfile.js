0e89ea5348d8d5a3731ac19ce23b7a46f12b7e4d
Tiago Porto <tiago@tiagoporto.com>
2017-02-04 18:35:28 -0200

    Fixed lint

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('coverall', function () {
 
 gulp.task('karma', function (done) {
   new Karma({
-    configFile: __dirname + '/karma.conf.js'
+    configFile: path.join(__dirname, '/karma.conf.js')
   }, done).start()
 })
 
