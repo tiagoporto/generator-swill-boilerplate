d4519901ec055ce91edc556e476ef82c8ceaed32
Tiago Porto <tiagoporto@ymail.com>
2017-10-16 22:53:13 -0200

    Add script command

---
+++ script/index.js
@@ -0,0 +1,32 @@
+var Yeoman = require('yeoman-generator')
+// var path = require('path')
+// var fs = require('fs')
+
+module.exports = class extends Yeoman {
+  prompting () {
+    var prompts = [{
+      name: 'filename',
+      required: true,
+      message: 'Filename'
+    }]
+
+    return this.prompt(prompts).then(function (props) {
+      this.prompts = props
+      this.props = {
+        'filename': props.filename
+      }
+    }.bind(this))
+  }
+
+  writing () {
+    // var config = require(this.destinationPath('config.json'))
+    // var extension = require(this.destinationPath('.yo-rc.json'))
+    // var savefile = path.join(config.basePaths.src, config.basePaths.styles.src, `${this.props.filename}.js`)
+    // var indexpath = path.join(config.basePaths.src, config.basePaths.styles.src, `components/_index.${extension}`)
+    // var index = fs.readFileSync(this.destinationPath(path.join(config.basePaths.src, config.basePaths.styles.src, `components/_index.${extension}`)), 'utf8')
+
+    // this.fs.write(this.destinationPath(indexpath), `${index}\n@import "_${this.props.filename}"`)
+
+    // this.fs.write(this.destinationPath(savefile), '')
+  }
+}
