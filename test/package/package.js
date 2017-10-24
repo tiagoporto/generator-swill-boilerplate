51382a38833e9d7668c467d26a5efb83eaf8aaf9
Tiago Porto <tiago@tiagoporto.com>
2017-10-24 17:02:27 -0200

    Add pending test

--- test/package/package.js
+++ test/package/package.js
@@ -3,7 +3,7 @@ var yoTest = require('yeoman-test')
 var path = require('path')
 var fs = require('fs')
 
-describe('Package.json File', function () {
+describe('Package.json File', () => {
   before(function () {
     return yoTest
       .run(path.join(__dirname, '../../app'))
@@ -44,5 +44,30 @@ describe('Package.json File', function () {
 
   it('Coveralls', function () {
     yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'coveralls.json'), 'utf-8'))
+  })
+})
+
+describe('Package.json File', function () {
+  before(function () {
+    return yoTest
+      .run(path.join(__dirname, '../../app'))
+      .withPrompts({
+        preprocessor: 'stylus',
+        features: [],
+        options: [],
+        files: [],
+        handlebars: false,
+        gitHooks: [
+          'prepush',
+          'precommit'
+        ],
+        integrations: [],
+        license: 'unlicense'
+      })
+      .toPromise()
+  })
+
+  xit('Githook', function () {
+    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'githooks.json'), 'utf-8'))
   })
 })
