bd3891e9c48ac9c5afddde04896fea94465e7620
Tiago Porto <tiagoporto@ymail.com>
2017-10-24 23:19:55 -0200

    Add pending tests to package.json

--- test/package/package.js
+++ test/package/package.js
@@ -20,8 +20,8 @@ describe('Package.json File', () => {
       .toPromise()
   })
 
-  it('Clean', function () {
-    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'clean.json'), 'utf-8'))
+  it('Stylus', function () {
+    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'stylus.json'), 'utf-8'))
   })
 })
 
@@ -69,5 +69,100 @@ describe('Package.json File', function () {
 
   xit('Githook', function () {
     yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'githooks.json'), 'utf-8'))
+  })
+})
+
+describe('Package.json File', function () {
+  before(function () {
+    return yoTest
+      .run(path.join(__dirname, '../../app'))
+      .withPrompts({
+        preprocessor: 'stylus',
+        features: [],
+        options: [],
+        files: [],
+        handlebars: true,
+        gitHooks: [],
+        integrations: [],
+        license: 'unlicense'
+      })
+      .toPromise()
+  })
+
+  xit('Handlebars', function () {
+    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'handlebars.json'), 'utf-8'))
+  })
+})
+
+describe('Package.json File', () => {
+  before(function () {
+    return yoTest
+      .run(path.join(__dirname, '../../app'))
+      .withPrompts({
+        preprocessor: 'sass',
+        features: [],
+        options: [],
+        files: [],
+        handlebars: false,
+        gitHooks: [],
+        integrations: [],
+        license: 'unlicense'
+      })
+      .toPromise()
+  })
+
+  xit('Sass', function () {
+    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'sass.json'), 'utf-8'))
+  })
+})
+
+describe('Package.json File', () => {
+  before(function () {
+    return yoTest
+      .run(path.join(__dirname, '../../app'))
+      .withPrompts({
+        preprocessor: 'stylus',
+        features: [
+          'jquery',
+          'normalize',
+          'outdatedBrowser'
+        ],
+        options: [],
+        files: [],
+        handlebars: false,
+        gitHooks: [],
+        integrations: [],
+        license: 'unlicense'
+      })
+      .toPromise()
+  })
+
+  xit('Optionals Libs', function () {
+    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'libs.json'), 'utf-8'))
+  })
+})
+
+describe('Package.json File', () => {
+  before(function () {
+    return yoTest
+      .run(path.join(__dirname, '../../app'))
+      .withPrompts({
+        preprocessor: 'stylus',
+        features: [
+          'jquery'
+        ],
+        options: [],
+        files: [],
+        handlebars: false,
+        gitHooks: [],
+        integrations: [],
+        jqueryLogoDownloadtip: true,
+        license: 'unlicense'
+      })
+      .toPromise()
+  })
+
+  xit('jqueryLogoDownloadtip', function () {
+    yoAssert.equalsFileContent('package.json', fs.readFileSync(path.resolve(__dirname, 'jqueryLogoDownloadtip.json'), 'utf-8'))
   })
 })
